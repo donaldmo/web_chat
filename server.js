a7d2498c03a1040c64d53f287fcc0d5e055b3a75
Donald Motswiri <domotswiri@gmail.com>
2024-02-17 05:34:13 +0200

    added cors to server.js

--- server.js
+++ server.js
@@ -2,39 +2,51 @@ const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const socket = require("socket.io");
-const io = socket(server);
+const socketIO = require("socket.io");
+const io = socketIO(server);
+const cors = require('cors');
+
+app.use(cors());
+
+// Serve static files (React build) from the 'client/build' folder
+app.use(express.static('client/build'));
+
+// Handle requests to the root URL by serving the React build index.html
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/build/index.html');
+});
 
 const rooms = {};
 
 io.on("connection", socket => {
-	socket.on("join room", roomID => {
-		if (rooms[roomID]) {
-			rooms[roomID].push(socket.id);
-		} else {
-			rooms[roomID] = [socket.id];
-		}
+  socket.on("join room", roomID => {
+    if (rooms[roomID]) {
+      rooms[roomID].push(socket.id);
+    } else {
+      rooms[roomID] = [socket.id];
+    }
 
-		const otherUser = rooms[roomID].find(id => id !== socket.id);
-		
-		if (otherUser) {
-			socket.emit("other user", otherUser);
-			socket.to(otherUser).emit("user joined", socket.id);
-		}
-	});
+    const otherUser = rooms[roomID].find(id => id !== socket.id);
 
-	socket.on("offer", payload => {
-		io.to(payload.target).emit("offer", payload);
-	});
+    if (otherUser) {
+      socket.emit("other user", otherUser);
+      socket.to(otherUser).emit("user joined", socket.id);
+    }
+  });
 
-	socket.on("answer", payload => {
-		io.to(payload.target).emit("answer", payload);
-	});
+  socket.on("offer", payload => {
+    io.to(payload.target).emit("offer", payload);
+  });
 
-	socket.on("ice-candidate", incoming => {
-		io.to(incoming.target).emit("ice-candidate", incoming.candidate);
-	});
+  socket.on("answer", payload => {
+    io.to(payload.target).emit("answer", payload);
+  });
+
+  socket.on("ice-candidate", incoming => {
+    io.to(incoming.target).emit("ice-candidate", incoming.candidate);
+  });
 });
 
-
-server.listen(8000, () => console.log('server is running on port 8000'));
+// Start the server on port 8000
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
